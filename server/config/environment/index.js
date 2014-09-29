0fdace74bf56aaaf6dedee84f2cccdbb0a5e7727
Mason <maseh87@gmail.com>
2014-09-29 15:35:27 -0700

    updated environment

---
+++ server/config/environment/index.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var path = require('path');
+var _ = require('lodash');
+
+function requiredProcessEnv(name) {
+  if(!process.env[name]) {
+    throw new Error('You must set the ' + name + ' environment variable');
+  }
+  return process.env[name];
+}
+
+var defaults = {
+  env: process.env.NODE_ENV,
+
+  //Root path to the server
+  root: path.normalize(__dirname + '/../../..'),
+
+  port: process.env.PORT || 4000,
+
+  seedDB: false,
+
+  secrets: {
+    jwt: process.env.JWT_SECRET
+  },
+
+  mongo: {
+    options: {
+      db: {
+        safe: true
+      }
+    }
+  },
+
+  twitter: {
+    clientID: process.env.TWITTER_ID || 'id',
+    clientSecret: process.env.TWITTER_SECRET || 'secret',
+    callbackURL: (process.env.DOMAIN || '') + '/auth/twitter/callback'
+  },
+
+  google: {
+    clientID: process.env.TWITTER_ID || 'id',
+    clientSecret: process.env.TWITTER_SECRET || 'secret',
+    callbackURL: (process.env.DOMAIN || '') + '/auth/google/callback'
+  }
+};
+
+module.exports = _.merge(defaults, require('./' + process.env.NODE_ENV + '.js') || {});
